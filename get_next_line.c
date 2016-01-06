8a51b69368c9df2c9af6d80cfffbd529894ff367
Nathan ROMPTEAUX <nromptea@e1r7p9.42.fr>
2016-01-06 17:51:24 +0100

    bla

--- get_next_line.c
+++ get_next_line.c
@@ -6,63 +6,89 @@
 /*   By: nromptea <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/10 14:38:11 by nromptea          #+#    #+#             */
-/*   Updated: 2015/12/17 16:56:34 by nromptea         ###   ########.fr       */
+/*   Updated: 2016/01/06 15:39:40 by nromptea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "get_next_line.h"
 
-char	*ft_read(int fd, char *buff, char *cpy_buff, char *rest)
+char	*ft_read_all(char *cpy_buff, int fd)
 {
-	int			ret;
+	char	*tmp;
+	char	buff[BUFF_SIZE + 1];
+	int		ret;
 
-	if ((ft_strchr(rest, '\n') == NULL) &&
-			(ret = read(fd, buff, BUFF_SIZE) > 0))
+	while (42)
 	{
-		cpy_buff = ft_strjoin(cpy_buff, rest);
-		while ((ft_strchr(buff, '\n') == NULL))
+		ret = read(fd, buff, BUFF_SIZE);
+		if (ret <= 0)
+			break ;
+		buff[ret] = '\0';
+		tmp = cpy_buff;
+		if (!(cpy_buff = (char *)malloc(sizeof(char) * (BUFF_SIZE + ret + 1))))
+			return (NULL);
+		if (tmp == NULL)
+			ft_strcpy(cpy_buff, buff);
+		else
 		{
-			if ((ret = read(fd, buff, O_RDONLY)) > 0)
-				cpy_buff = ft_strjoin(cpy_buff, buff);
-			else
-				return (NULL);
+			ft_strcpy(cpy_buff, tmp);
+			ft_strcat(cpy_buff, buff);
+			free(tmp);
 		}
-		return (cpy_buff);
 	}
-	else if ((ft_strchr(rest, '\n') != NULL))
-		return (rest);
-	return (NULL);
+	return (cpy_buff);
 }
 
-char	*ft_choppe_first(char *cpy_buff)
+char	*first_line(char *cpy_buff)
 {
+	int		i;
 	char	*line;
-	int		len;
 
-	len = 0;
-	while (cpy_buff[len] != '\n')
-		len++;
-	line = ft_strnew(len - 1);
-	line = ft_strncpy(line, cpy_buff, len - 1);
+	i = 0;
+	while (cpy_buff[i] != '\n')
+		i++;
+	line = ft_strsub(cpy_buff, 0, i);
 	return (line);
+}
+
+char	*cut_line(char *cpy_buff)
+{
+	int		len;
+	int		i;
+	char	*rest;
+
+	len = ft_strlen(cpy_buff);
+	while (cpy_buff[i] != '\n')
+		i++;
+	rest = ft_strsub(cpy_buff, (i + 1), len);
+	return (rest);
 }
 
 int		get_next_line(int const fd, char **line)
 {
-	char		*buff;
-	static char	*rest = NULL;
 	char		*cpy_buff;
+	static char	*rest = NULL;
 
 	cpy_buff = NULL;
-	buff = ft_strnew(BUFF_SIZE);
-	if (fd < 0 || line == NULL || buff == NULL)
-		return (-1);
-	cpy_buff = ft_read(fd, buff, cpy_buff, rest);
-	if (cpy_buff == NULL)
-		return (0);
-	rest = ft_strdup((ft_strchr(cpy_buff, '\n')) + 1);
-	*line = ft_choppe_first(cpy_buff);
-	ft_strdel(cpy_buff);
+	cpy_buff = ft_read_all(cpy_buff, fd);
+	*line = first_line(cpy_buff);
+//	rest = cut_line(cpy_buff);
 	return (1);
+}
+
+int	main(int argc, char **argv)
+{
+	int	fd;
+	int	ret;
+	char	*line;
+	char	*cpy;
+
+	if (argc)
+	{
+		fd = open(argv[1], O_RDONLY);
+		ret = get_next_line(fd, &line);
+		ft_putstr(line);
+	}
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,18 @@
 /*   By: nromptea <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/17 16:23:11 by nromptea          #+#    #+#             */
-/*   Updated: 2015/12/17 16:26:08 by nromptea         ###   ########.fr       */
+/*   Updated: 2016/01/06 14:09:03 by nromptea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 42
+# define BUFF_SIZE 42 
 
-char	*ft_read(int fd, char *buff, char *cpy_buff, char *rest);
-char	*ft_choppe_first(char *cpy_buff);
+char	*ft_read_all(char *cpy_buff, int fd);
+char	*first_line(char *cpy_buff);
+char	*cut_line(char *cpy_buff);
 int		get_next_line(int const fd, char **line);
 
 #endif
